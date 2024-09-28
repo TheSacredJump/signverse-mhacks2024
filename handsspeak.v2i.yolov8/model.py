db15ed06e30559ed35769532fce83d29afe7c35b
Sumit Bahadur <sumitbahadur@Sumits-MacBook-Air-2.local>
2024-09-28 15:38:45 -0400

    testing

--- handsspeak.v2i.yolov8/model.py
+++ handsspeak.v2i.yolov8/model.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.yaml') 
 
 # replace with your path
-model.train(data='/Users/sumitbahadur/Mhacks 2024/speaky-mhacks2024/handsspeak.v2i.yolov8/data.yaml', epochs=100, imgsz=640, )
+model.train(data='/Users/sumitbahadur/Mhacks 2024/speaky-mhacks2024/handsspeak.v2i.yolov8/data.yaml', epochs=100, imgsz=640, device='cpu', project='/Users/sumitbahadur/Mhacks 2024/speaky-mhacks2024/handsspeak.v2i.yolov8')
